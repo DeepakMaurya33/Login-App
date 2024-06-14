eda5fd7e935aa8ef9abe165bf8e9405fc1fdee79
Deepak Maurya <156901926+DeepakMaurya33@users.noreply.github.com>
2024-06-15 01:00:58 +0530

    Add files via upload

---
+++ src/main/java/com/example/loginapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.loginapp;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText usernameInput;
+    EditText passwordInput;
+    Button loginBtn;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameInput = findViewById(R.id.username_input);
+        passwordInput = findViewById(R.id.password_input);
+        loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                Log.i("Test Credentials", "Username: " + username + " and password: " + password);
+            }
+        });
+    }
+}
+
